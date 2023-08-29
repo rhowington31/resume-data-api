98308a242eafcf3cea5a2db82168f947d6fcd8a7
Ryan Howington <rhowington31@gmail.com>
2023-08-28 22:31:39 -0500

    fixed online_resume and replaced it with online_resume_url

--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -14,7 +14,7 @@ test "show" do
   assert_response 200
 
     data = JSON.parse(response.body)
-    assert_equal ["id", "first_name", "last_name", "email", "phone_number", "short_bio", "linkedin_url", "twitter_handle", "website_url", "online_resume", "github_url", "photo", "created_at", "updated_at"], data.keys
+    assert_equal ["id", "first_name", "last_name", "email", "phone_number", "short_bio", "linkedin_url", "twitter_handle", "website_url", "online_resume_url", "github_url", "photo", "created_at", "updated_at"], data.keys
   end
 
 end
